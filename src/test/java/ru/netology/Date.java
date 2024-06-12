68bd268102a527337050ffd0b088e31ec7ce6ac3
QAVshivtsev <159697104+QAVshivtsev@users.noreply.github.com>
2024-06-12 13:55:25 +0300

    Change Test

--- src/test/java/ru/netology/Date.java
+++ src/test/java/ru/netology/Date.java
@@ -6,6 +6,6 @@ import java.time.format.DateTimeFormatter;
 public class Date {
     public static String getDate(int daysAdd) {
         LocalDate currentDate = LocalDate.now().plusDays(daysAdd);
-        return currentDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        return currentDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 }
--- src/test/java/ru/netology/DeliveryCard.java
+++ src/test/java/ru/netology/DeliveryCard.java
@@ -1,36 +1,30 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
 import java.time.Duration;
-
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.Keys.TAB;
 import static ru.netology.Date.getDate;
 
 
-    class DeliveryCard {
-        @Test
-        void shouldDeliveryFormWorks() {
-            Configuration.holdBrowserOpen = true;
-            open("http://localhost:9999/");
-            $("span[data-test-id = city]").click();
-            $("input[placeholder=Город]").setValue("Нижний Новгород");
-            $("span[class=menu-item__control]").click();
-            $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-            String expectedDate = getDate(4);
-            $x("//input[@placeholder='Дата встречи']").setValue(expectedDate).sendKeys(TAB);;
-            $("span[data-test-id=name]").click();
-            $("input[name=name]").setValue("Вшивцев Владислав");
-            $("span[data-test-id=phone]").click();
-            $("input[name=phone]").setValue("+79875358135");
-            $("label[data-test-id=agreement]").click();
-            $x("//span[contains(text(), 'Забронировать')]").click();
-            $(withText("Успешно!")).should(appear, Duration.ofSeconds(15));
-            $x("//div[contains(text(), 'Успешно!')]");
-        }
+class DeliveryCard {
+    @Test
+    void shouldDeliveryFormWorks() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("input[placeholder=Город]").setValue("Нижний Новгород");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
+        String expectedDate = getDate(4);
+        $x("//input[@placeholder='Дата встречи']").setValue(expectedDate).sendKeys(TAB);
+        $("input[name=name]").setValue("Вшивцев Владислав");
+        $("input[name=phone]").setValue("+79875358135");
+        $("label[data-test-id=agreement]").click();
+        $x("//span[contains(text(), 'Забронировать')]").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + expectedDate));
     }
+}
